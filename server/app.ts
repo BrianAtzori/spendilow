4ad8bf38ecfe05501840dad746727e7a2a5643c7
BrianAtzori <brianatzori@gmail.com>
2023-11-15 07:47:13 +0100

    Structuring app.ts with base structure

--- server/app.ts
+++ server/app.ts
@@ -1,32 +1,86 @@
-import { Express, Request, Response } from 'express'
+// ------ ENV SETUP ------
+require("dotenv").config();
+
+// ------ EXPRESS SETUP ------
+import { Express, Request, Response, RequestHandler } from 'express' //TS Import
 const express = require('express');
 const app: Express = express();
-const dotenv = require('dotenv');
-dotenv.config();
-const pool = require('./db')
+require("express-async-errors");
+app.use(express.json());
 
-app.get('/users', async (req: Request, res: Response) => {
+// ----- DB IMPORT ------
+const pool = require('./db/db')
+
+//------ SECURITY SETUP ------
+//Imports
+const helmet = require("helmet");
+const cors = require("express-cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+//Activation
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("Trust Proxy", 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 100,
+    max: 100,
+}))
+
+// ------ MIDDLEWARE SETUP ------
+//Imports
+// const errorHandlerMiddleware: RequestHandler = require("./middleware/error-handler");
+/**
+ * TODO: Da inserire e attivare eventuali altri middleware
+ */
+
+//Activation
+// app.use(errorHandlerMiddleware);
+
+//------ ROUTES SETUP ------
+//const authRouter = require("./routes/auth"); ESEMPIO
+/**
+ * TODO: Da inserire/importare e mappare eventuali altre rotte
+ */
+
+//------- Try DB Connection or throw error ------
+
+const start = async () => {
     let conn;
     try {
-        // establish a connection to MariaDB
         conn = await pool.getConnection();
-
-        // create a new query
-        var query = "select * from splusers";
-
-        // execute the query and set the result to a new variable
-        var rows = await conn.query(query);
-
-        // return the results
-        res.send(rows);
-    } catch (err) {
-        throw err;
-    } finally {
+        if (conn) {
+            app.listen(process.env.PORT, () => {
+                console.log(`Server connected to DB and running at http://localhost:${process.env.PORT}`);
+            });
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+    finally {
         if (conn) return conn.release();
     }
-});
+}
 
-app.listen(process.env.PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${process.env.PORT}`);
-});
+start();
 
+// app.get('/users', async (req: Request, res: Response) => {
+//     let conn;
+//     try {
+//         // establish a connection to MariaDB
+//         conn = await pool.getConnection();
+
+//         // create a new query
+//         var query = "select * from splusers";
+
+//         // execute the query and set the result to a new variable
+//         var rows = await conn.query(query);
+
+//         // return the results
+//         res.send(rows);
+//     } catch (err) {
+//         throw err;
+//     }
+// });
--- server/dist/app.js
+++ server/dist/app.js
@@ -9,31 +9,74 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// ------ ENV SETUP ------
+require("dotenv").config();
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-const pool = require('./db');
-app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+require("express-async-errors");
+app.use(express.json());
+// ----- DB IMPORT ------
+const pool = require('./db/db');
+//------ SECURITY SETUP ------
+//Imports
+const helmet = require("helmet");
+const cors = require("express-cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+//Activation
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("Trust Proxy", 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 100,
+    max: 100,
+}));
+// ------ MIDDLEWARE SETUP ------
+//Imports
+// const errorHandlerMiddleware: RequestHandler = require("./middleware/error-handler");
+/**
+ * TODO: Da inserire e attivare eventuali altri middleware
+ */
+//Activation
+// app.use(errorHandlerMiddleware);
+//------ ROUTES SETUP ------
+//const authRouter = require("./routes/auth"); ESEMPIO
+/**
+ * TODO: Da inserire/importare e mappare eventuali altre rotte
+ */
+//------- Try DB Connection or throw error ------
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
     let conn;
     try {
-        // establish a connection to MariaDB
         conn = yield pool.getConnection();
-        // create a new query
-        var query = "select * from splusers";
-        // execute the query and set the result to a new variable
-        var rows = yield conn.query(query);
-        // return the results
-        res.send(rows);
+        if (conn) {
+            app.listen(process.env.PORT, () => {
+                console.log(`Server connected to DB and running at http://localhost:${process.env.PORT}`);
+            });
+        }
     }
-    catch (err) {
-        throw err;
+    catch (error) {
+        console.log(error);
     }
     finally {
         if (conn)
             return conn.release();
     }
-}));
-app.listen(process.env.PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${process.env.PORT}`);
 });
+start();
+// app.get('/users', async (req: Request, res: Response) => {
+//     let conn;
+//     try {
+//         // establish a connection to MariaDB
+//         conn = await pool.getConnection();
+//         // create a new query
+//         var query = "select * from splusers";
+//         // execute the query and set the result to a new variable
+//         var rows = await conn.query(query);
+//         // return the results
+//         res.send(rows);
+//     } catch (err) {
+//         throw err;
+//     }
+// });
