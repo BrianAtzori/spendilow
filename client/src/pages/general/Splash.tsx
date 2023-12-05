0c3162508ab0fe2236280753c8d1b540abce57b2
BrianAtzori <brianatzori@gmail.com>
2023-12-05 14:00:54 +0100

    Fix before dev start

--- client/src/pages/general/Splash.tsx
+++ client/src/pages/general/Splash.tsx
@@ -1,22 +1,23 @@
 // ------ REACT ------
 import React from "react";
-import { Link } from "react-router-dom";
 
 // ------ COMPONENTS & PAGES ------
 import Loader from "../../components/shared/Loader";
 
 // ------ REDUX ------
-import { useAppSelector, useAppDispatch } from "../../redux/hooks";
-import { changeState } from "../../redux/reducers/main/mainSlice";
+// import { useAppSelector, useAppDispatch } from "../../redux/hooks";
+// import { changeState } from "../../redux/reducers/main/mainSlice";
 
-//! This component manage user redirection, if server is alive check if user is logged, if it's logged go to app or go to auth picker
+//* This component manage user redirection, if server is alive check if user is logged, if it's logged go to app or go to auth picker
 export default function Splash() {
-  const result: boolean = useAppSelector((state) => state.main.value);
-  const dispatch = useAppDispatch();
+  // const result: boolean = useAppSelector((state) => state.main.value);
+  // const dispatch = useAppDispatch();
 
   return (
     <>
-      <Loader
+      {/** ------ INITIAL TESTING ------ */}
+
+      {/* <Loader
         isLoading={result}
         message={"Controllo il collegamento ai server di Spendilow 💰"}
       ></Loader>
@@ -32,7 +33,13 @@ export default function Splash() {
         <h1 className="font-primary">Titolo</h1>
         <h2 className="font-heading">Titoletto</h2>
         <p className="font-body">Paragrafo</p>
-      </div>
+      </div> */}
+
+      <Loader
+        isLoading={true}
+        message={"Controllo il collegamento ai server di Spendilow 💰"}
+      ></Loader>
+      
     </>
   );
 }
