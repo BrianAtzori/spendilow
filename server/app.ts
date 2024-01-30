41537c8558a262170c7df3321161d0799c9768dc
BrianAtzori <brianatzori@gmail.com>
2024-01-30 16:19:32 +0100

    Pushing Correct Origin

--- server/app.ts
+++ server/app.ts
@@ -29,7 +29,7 @@ app.use(helmet());
 app.use(cors({
     // origin: 'http://localhost:5173',
     // origin: '*',
-    origin: 'https://spendilow.netlify.app/',
+    origin: 'https://spendilow.netlify.app',
     credentials: true
 }));
 app.use(xss());
