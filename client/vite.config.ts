4682bf2497bdc7b654a03a8bbcd68c1773b4bef4
Brian Atzori <43118219+BrianAtzori@users.noreply.github.com>
2024-03-27 18:32:22 +0100

    Trying to build react app to nginx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    host: true, // needed for the Docker Container port mapping to work
-    port: 5173, // you can replace this port with any port
+    // host: true, // needed for the Docker Container port mapping to work
+    // port: 80, // you can replace this port with any port
   },
 });
