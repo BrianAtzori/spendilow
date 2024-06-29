98e92160d0f3e917a0b6ee2a0aec2e9e2d890cd3
BrianAtzori <brianatzori@gmail.com>
2024-06-29 15:22:04 +0200

    Ignored dist folder

--- server/dist/controllers/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { BadRequestError, UnauthenticatedError } = require("../errors");
-// ------ Exports ------
-module.exports = {};
--- server/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const authRouter = express.Router();
-// ------ Controllers imports and setup ------
-const {} = require("../controllers/auth");
-// ------ Exports ------
-module.exports = authRouter;
